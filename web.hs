a7c7cd66eda2c74a382057ad59f7cc066a6afea6
lep <lep.delete@googlemail.com>
2019-10-28 18:11:18 +0100

    Quick and dirty web api for search
    Added a quick and dirty web api which returns the results for search
    via json using scotty as a webframework.

---
+++ web.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Web.Scotty
+
+import Data.Monoid (mconcat)
+
+import Jassbot.Search
+import Jassbot.DB
+import Jassbot.Signature
+
+import Data.Binary (decodeFileOrFail, encodeFile)
+
+import System.Directory
+import System.IO (hPutStrLn, stderr)
+import System.IO.Error
+import System.Exit
+import System.FilePath ((</>))
+
+import Control.Monad.IO.Class (liftIO)
+import Control.Arrow
+
+import Data.Aeson.Types
+
+import qualified Jass.Ast
+
+instance ToJSON Jass.Ast.Constant
+instance ToJSON Native
+instance ToJSON Signature
+
+getDbPath :: Maybe String -> IO String
+getDbPath x =
+  case x of
+    Just datadir -> return datadir
+    Nothing -> getXdgDirectory XdgData "jassbot"
+
+readDb :: Maybe String -> IO DB
+readDb p = do
+    datadir <- getDbPath p
+    x <- tryIOError (decodeFileOrFail $ datadir </> "jassbot.db")
+    case x of
+        Left ex -> do
+            hPutStrLn stderr $ unwords ["Could not open database. Have you run init yet?", show ex]
+            exitWith $ ExitFailure 1
+        Right x' ->
+          case x' of
+            Right x' -> return x'
+            Left (_, msg) -> do
+                hPutStrLn stderr $ unwords ["Could not open database. Have you run init yet?", msg]
+                exitWith $ ExitFailure 1
+
+main = do
+    db <- readDb (Just ".")
+    scotty 3000 $
+        get "/api" $
+            param "query" >>= json . take 20 . (search db `flip` 0.4)
+
